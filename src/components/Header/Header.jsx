0561b723c912c4682a36f82a935bbdc5f32b2d71
diegonacif <diegoreisnacif@gmail.com>
2023-05-09 11:52:09 -0300

    refactor: header logo and signout button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { HeaderContainer, HeaderContent } from "./styles";
 import { CalendarX , SignOut} from "phosphor-react";
 import { AuthEmailContext } from "../../contexts/AuthEmailProvider";
+import logoImg from "../../assets/event-logo-only.png";
 
 
 export const Header = () => {
@@ -9,9 +10,16 @@ export const Header = () => {
   return (
     <HeaderContainer>
       <HeaderContent>
-        <CalendarX size={60} />
-        <strong>Create and Schedule Your Event</strong>
-        <SignOut onClick={() => logoutUser()} size={35}/>
+        <div className="logo-img-wrapper">
+          <img src={logoImg} alt="" id="logoImgBg" />
+          <img src={logoImg} alt="" id="logoImg" />
+        </div>
+        <span id="logo-title">EVENT SCHEDULER</span>
+        <div className="logout-wraper">
+          <SignOut onClick={() => logoutUser()} weight="bold" size={28}/>
+        </div>
+        {/* <CalendarX size={60} /> */}
+        {/* <strong>Create and Schedule Your Event</strong> */}
       </HeaderContent>
       
     </HeaderContainer>
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,20 +1,68 @@
 import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
-  background: linear-gradient(135deg, var(--gray-700) 0%, var(--red) 50%, var(--orange) 100%);
-  padding: 2.5rem 0 7.5rem;
+  display: flex;
+  width: 100vw;
+  height: 7rem;
+  background: linear-gradient(135deg, var(--gray-700) 0%, var(--red) 60%, var(--orange) 100%);
+  padding: 0 0.75rem;
 `;
 
 export const HeaderContent = styled.div`
-  width: 100%;
-  max-width: 1120px;
-  margin: 0 auto;
-  padding: 0 1.5rem;
-
+  position: relative;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 0.5rem;
+  width: 100%;
+
+  .logo-img-wrapper {
+    position: relative;
+    height: 3.5rem;
+
+    #logoImg {
+      position: relative;
+      height: 100%;
+      z-index: 10;
+    }
+    #logoImgBg {
+      position: absolute;
+      top: 2px;
+      left: 2px;
+      height: 100%;
+      mix-blend-mode: color-burn;
+      filter: brightness(0.5) blur(3px);
+      z-index: 5;
+    }
+  }
+
+  #logo-title {
+    color: white;
+    font-size: var(--text-xl);
+    margin-top: 0.5rem;
+    text-shadow: 2px 2px 2px #00000060;
+    /* font-family: 'Cherry Cream Soda', cursive; */
+  }
+
+  .logout-wraper {
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 3em;
+    height: 3rem;
+    border-radius: 9999px;
+    transition: background-color 0.1s;
+
+    /* &:hover {
+      background-color: #00000040;
+      transition: background-color 0.3s;
+    } */
+  }
+
 
   strong {
     font-size: large;
